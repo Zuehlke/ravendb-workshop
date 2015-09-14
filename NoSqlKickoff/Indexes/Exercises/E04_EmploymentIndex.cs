88b5b60104aca9800588ca6d3411081cc852d61d
Roman Kuczynski <rkutschi@gmail.com>
2015-09-14 17:56:30 +0200

    implement exercise 4

---
+++ NoSqlKickoff/Indexes/Exercises/E04_EmploymentIndex.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Raven.Client.Indexes;
+
+namespace NoSqlKickoff.Indexes.Exercises
+{
+    using NoSqlKickoff.Model;
+
+    public class E04_EmploymentIndex : AbstractIndexCreationTask<Employment>
+    {
+        public class IndexEntry
+        {
+            public string TeamName { get; set; }
+
+            public string Season { get; set; }
+        }
+        
+        public E04_EmploymentIndex()
+        {
+            Map = employments => from employment in employments
+                                 let team = LoadDocument<Team>(employment.TeamId)
+                                 select new IndexEntry
+                                            {
+                                                Season = employment.Season,
+                                                TeamName = team.Name
+                                            };
+        }
+    }
+}
---
+++ NoSqlKickoff/Transformers/PlayerEmploymentTransformer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoSqlKickoff.Transformers
+{
+    using NoSqlKickoff.Model;
+
+    using Raven.Client.Indexes;
+
+    public class PlayerEmploymentTransformer : AbstractTransformerCreationTask<Employment>
+    {
+        public PlayerEmploymentTransformer()
+        {
+            TransformResults = employments => from employment in employments
+                                              let player = LoadDocument<Player>(employment.PlayerId)
+                                              select
+                                                  new PlayerEmployment()
+                                                      {
+                                                          FirstName = player.FirstName,
+                                                          LastName = player.LastName
+                                                      };
+        }
+    }
+}
