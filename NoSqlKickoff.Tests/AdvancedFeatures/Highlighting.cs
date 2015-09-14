f87df7c0504bbda5f04393afdf0c0c3a96d753bb
Tobias Zürcher <tobias.zuercher@gmail.com>
2015-09-14 16:19:24 +0200

    add highlighting & suggestions

---
+++ NoSqlKickoff.Tests/AdvancedFeatures/Highlighting.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using Lucene.Net.Search.Vectorhighlight;
+using NoSqlKickoff.Indexes;
+using NoSqlKickoff.Indexes.AdvancedFeatures;
+using NoSqlKickoff.Model;
+using NUnit.Framework;
+using Raven.Client;
+using Raven.Client.Indexes;
+using Raven.Tests.Helpers;
+
+namespace NoSqlKickoff.Tests.AdvancedFeatures
+{
+    public class Highlighting : RavenTestBase
+    {
+        private IDocumentStore _store;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _store = NewDocumentStore();
+            IndexCreation.CreateIndexes(typeof(Player_Index_R03).Assembly, _store);
+        }
+
+        [Test]
+        public void Highlighting_Demo()
+        {
+            InsertPlayers();
+
+            using (var session = _store.OpenSession())
+            {
+                FieldHighlightings highlightings;
+
+                var results = session
+                    .Advanced
+                    .DocumentQuery<Player, Player_Index_Suggestions>()
+                    .Highlight("FirstName", 18, 1, out highlightings)
+                    .SetHighlighterTags("<b>", "</b>")
+                    .Search("FirstName", "ma*")
+                    .ToList();
+
+                var builder = new StringBuilder().AppendLine("<ul>");
+
+                foreach (var result in results)
+                {
+                    var fragments = highlightings.GetFragments(result.Id);
+                    builder.AppendLine(string.Format("  <li>{0}</li>", fragments.First()));
+                }
+
+                string ul = builder.AppendLine("</ul>").ToString();
+                
+                Console.WriteLine(ul);
+            }
+        }
+
+        private void InsertPlayers()
+        {
+            using (var session = _store.OpenSession())
+            {
+                var players = PlayerGenerator.GetPlayers();
+                foreach (var player1 in players)
+                {
+                    session.Store(player1);
+                }
+
+                session.SaveChanges();
+            }
+
+            WaitForIndexing(_store);
+        }
+    }
+}
---
+++ NoSqlKickoff/Indexes/AdvancedFeatures/Player_Index_Suggestions.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using NoSqlKickoff.Model;
+using Raven.Abstractions.Indexing;
+using Raven.Client.Indexes;
+
+namespace NoSqlKickoff.Indexes.AdvancedFeatures
+{
+    public class Player_Index_Suggestions : AbstractIndexCreationTask<Player>
+    {
+        public class IndexEntry
+        {
+            public string FirstName { get; set; }
+        }
+
+        public Player_Index_Suggestions()
+        {
+            Map = players => from player in players
+                             select new IndexEntry
+                             {
+                                 FirstName = player.FirstName
+                             };
+
+            Indexes.Add(p => p.FirstName, FieldIndexing.Analyzed);
+
+            Store(x => x.FirstName, FieldStorage.Yes);
+            Suggestion(p => p.FirstName, new SuggestionOptions());
+            
+            TermVector(p => p.FirstName, FieldTermVector.WithPositionsAndOffsets); // just used for highlighting
+        }
+    }
+}
