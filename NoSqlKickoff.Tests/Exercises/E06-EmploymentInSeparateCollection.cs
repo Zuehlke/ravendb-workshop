e5ca4e1202189f8e59b1bcc0deb8da8b2fdba3e3
Michael Zoellner <michael.zoellner@uni-konstanz.de>
2015-09-15 22:03:00 +0200

    split up test classes

---
+++ NoSqlKickoff.Tests/Exercises/E06-EmploymentInSeparateCollection.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using NoSqlKickoff.Indexes;
+using NoSqlKickoff.Indexes.Exercises;
+using NoSqlKickoff.Model;
+using NoSqlKickoff.Transformers;
+
+using NUnit.Framework;
+
+using Raven.Abstractions.Data;
+using Raven.Client;
+using Raven.Client.Indexes;
+using Raven.Client.Linq;
+using Raven.Tests.Helpers;
+
+using ServiceStack.Text;
+
+namespace NoSqlKickoff.Tests.Exercises
+{
+    public class E06_EmploymentInSeparateCollection : RavenTestBase
+    {
+        private IDocumentStore _store;
+        
+        /// <summary>
+        /// TODO: Exercise 9
+        /// As a user I want to know what players have been employed by "Borussia Dortmund" in season "2013-2014".
+        /// </summary>
+        public List<PlayerEmployment> FindPlayerEmploymentsUsingJoin()
+        {
+            using (var session = _store.OpenSession())
+            {
+                return session.Query<E06_EmploymentIndex.IndexEntry, E06_EmploymentIndex>()
+                    .Where(x => x.Season == "2013-2014" && x.TeamName == "Borussia Dortmund")
+                    .TransformWith<PlayerEmploymentTransformer, PlayerEmployment>().ToList();
+            }
+        }
+
+        [Test]
+        public void FindPlayerEmploymentsUsingJoin_ShouldReturnAllPlayersOfDortmundIn1314()
+        {    
+            var playerEmployments = FindPlayerEmploymentsUsingJoin();
+
+            playerEmployments.PrintDump();
+
+            //TODO: Update Assert when season data is correct
+            Assert.That(playerEmployments.Count, Is.AtLeast(1));
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            _store = NewDocumentStore();
+
+            IndexCreation.CreateIndexes(typeof(Player_Index_R03).Assembly, _store);
+
+            var playerList = DataGenerator.CreatePlayerList();
+            var teamList = DataGenerator.CreateTeamList();
+            var employmentList = DataGenerator.CreateEmploymentList();
+
+            using (var bulkInsert = _store.BulkInsert(null, new BulkInsertOptions { OverwriteExisting = true }))
+            {
+                foreach (var player in playerList)
+                {
+                    bulkInsert.Store(player);
+                }
+
+                foreach (var team in teamList)
+                {
+                    bulkInsert.Store(team);
+                }
+
+                foreach (var employment in employmentList)
+                {
+                    bulkInsert.Store(employment);
+                }
+            }
+
+            WaitForIndexing(_store);
+        }
+    }
+}
--- NoSqlKickoff/Indexes/Exercises/E06_EmploymentIndex.cs
+++ NoSqlKickoff/Indexes/Exercises/E06_EmploymentIndex.cs
@@ -1,25 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
+
+using NoSqlKickoff.Model;
+
 using Raven.Client.Indexes;
 
 namespace NoSqlKickoff.Indexes.Exercises
 {
-    using NoSqlKickoff.Model;
-
     public class E06_EmploymentIndex : AbstractIndexCreationTask<Employment>
     {
         public class IndexEntry
         {
             public string TeamName { get; set; }
-
             public string Season { get; set; }
         }
         
         public E06_EmploymentIndex()
         {
+            // TODO: Create Map property for Employment Index
             Map = employments => from employment in employments
                                  let team = LoadDocument<Team>(employment.TeamId)
                                  select new IndexEntry
