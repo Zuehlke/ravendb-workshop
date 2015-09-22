485f98faff65f4683db8cdabe24e331f432f5f27
Michael Zoellner <michael.zoellner@uni-konstanz.de>
2015-09-22 21:36:16 +0200

    E04: Exercise 7 and 8

---
+++ NoSqlKickoff.Tests/Exercises/E04-QueryStaticIndexWithTypeCoercion.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using NoSqlKickoff.Indexes.Exercises;
+using NoSqlKickoff.Model.Exercises;
+using NoSqlKickoff.Tests.Reference;
+
+using NUnit.Framework;
+
+using Raven.Client;
+using Raven.Client.Indexes;
+using Raven.Tests.Helpers;
+
+using ServiceStack.Text;
+
+namespace NoSqlKickoff.Tests.Exercises
+{
+    [TestFixture]
+    public class E04_QueryStaticIndexWithTypeCoercion : RavenTestBase
+    {
+        private IDocumentStore _store;
+
+        /// <summary>
+        /// TODO: Exercise 7
+        /// As a user I want to find the player "Christiano Ronaldo" by querying the full name
+        /// </summary>
+        /// <returns>
+        /// A player object of Christiano
+        /// </returns>
+        /// <remarks>
+        /// http://ravendb.net/docs/article-page/3.0/csharp/client-api/session/querying/how-to-perform-projection#oftype-(as)---simple-projection
+        /// </remarks>
+        /// <see cref="R04_Querying_TypeCoercion"/>
+        public Player FindChristiano()
+        {
+            // HINT: Query()
+            // HINT: OfType()
+
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// TODO: Exercise 8
+        /// As a user I want to find all players that have the Nationality "Brazil"
+        /// </summary>
+        /// <returns>
+        /// A list of players who have the nationality "Brazil"
+        /// </returns>
+        /// <returns>A list of brazilian players</returns>
+        /// <remarks>
+        /// http://ravendb.net/docs/article-page/3.0/csharp/client-api/session/querying/how-to-perform-projection#oftype-(as)---simple-projection
+        /// </remarks>
+        /// <see cref="R04_Querying_TypeCoercion"/>
+        public List<Player> FindBrazilianPlayers()
+        {
+            // HINT: Query()
+            // HINT: OfType()
+
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void FindChristiano_ShouldReturnChristiano()
+        {
+            var christiano = FindChristiano();
+
+            christiano.PrintDump();
+
+            Assert.That(christiano.LastName, Is.EqualTo("Ronaldo"));
+        }
+
+        [Test]
+        public void FindBrazilianPlayers_ShouldReturnAListOfBrazilianPlayers()
+        {
+            var brazilisanPlayers = FindBrazilianPlayers();
+
+            brazilisanPlayers.PrintDump();
+
+            Assert.That(brazilisanPlayers.Count, Is.AtLeast(1));
+            Assert.That(brazilisanPlayers.All(p => p.Nationality.Name == "Brazil"), Is.True);
+        }
+
+        [TestFixtureSetUp]
+        public void TestFixtureSetUp()
+        {
+            _store = NewDocumentStore();
+
+            IndexCreation.CreateIndexes(typeof(Player).Assembly, _store);
+
+            var players = DataGenerator.CreatePlayerList();
+
+            using (var bulkInsert = _store.BulkInsert())
+            {
+                foreach (var player in players)
+                {
+                    bulkInsert.Store(player);
+                }
+            }
+
+            WaitForIndexing(_store);
+        }
+    }
+}
---
+++ NoSqlKickoff/Indexes/Exercises/E04_PlayerIndex.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+
+using NoSqlKickoff.Indexes.Reference;
+using NoSqlKickoff.Model.Exercises;
+
+using Raven.Client.Indexes;
+
+namespace NoSqlKickoff.Indexes.Exercises
+{
+    /// <summary>
+    /// Player Index for E04_QueryStaticIndexWithTypeCoercion
+    /// </summary>
+    /// <remarks>
+    /// http://ravendb.net/docs/article-page/3.0/csharp/indexes/map-indexes#combining-multiple-fields-together
+    /// </remarks>
+    /// <see cref="Player_Index_R04"/>
+    public class E04_PlayerIndex : AbstractIndexCreationTask<Player, E04_PlayerIndex.IndexEntry>
+    {
+        public class IndexEntry
+        {
+            public string FullName { get; set; }
+
+            public string Nationality { get; set; }
+        }
+
+        public E04_PlayerIndex()
+        {
+            // TODO: Implement the Map property
+            // HINT: players => from player in players
+        }
+    }
+}
