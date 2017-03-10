c781ed4a7a2b47082f48eea198f1ffe430eca7bd
Michael Zöllner <michael.zoellner@mailbox.org>
2017-03-10 10:44:34 +0100

    update to RavenDB 3.5 and nunit 3.6

--- NoSqlKickoff.Tests/Exercises/E08-EmploymentCopyEmbeddedInPlayer.cs
+++ NoSqlKickoff.Tests/Exercises/E08-EmploymentCopyEmbeddedInPlayer.cs
@@ -51,8 +51,8 @@ namespace NoSqlKickoff.Tests.Exercises
             Assert.That(players.Count, Is.EqualTo(4));
         }
         
-        [TestFixtureSetUp]
-        public void TestFixtureSetUp()
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
         {
             _store = NewDocumentStore();
 
